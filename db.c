c64a936385dd68249abe9ac04852da62da0f76d0
tinyboy186 <tinyboy186@gmail.com>
2018-08-07 14:14:08 +0700

    Fix bug EXECUTE_DUPLICATE_KEY

--- db.c
+++ db.c
@@ -808,12 +808,12 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
-  void* node = get_page(table->pager, table->root_page_num);
-  uint32_t num_cells = (*leaf_node_num_cells(node));
-
   Row* row_to_insert = &(statement->row_to_insert);
   uint32_t key_to_insert = row_to_insert->id;
   Cursor* cursor = table_find(table, key_to_insert);
+
+  void *node = get_page(table->pager, cursor->page_num);
+  uint32_t num_cells = *leaf_node_num_cells(node);
 
   if (cursor->cell_num < num_cells) {
     uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
